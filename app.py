4971815c03e51f413343146dbd205d4e2f4075b6
petejmoore-ui <pete.j.moore@gmail.com>
2026-03-28 08:57:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3435,6 +3435,14 @@ document.addEventListener('keydown', function(e) { if (e.key === 'Escape') close
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-C1YNKZS6PG"></script>
 <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments)}gtag('js',new Date());gtag('config','G-C1YNKZS6PG');</script>
 
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w2r1ficsx2");
+</script>
+
 </body>
 </html>"""
 
